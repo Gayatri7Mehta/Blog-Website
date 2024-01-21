b60e96adc5ca45111f170a4bcb1f4b39c0d10091
Gayatri <gayatri29mehta@gmail.com>
2024-01-21 17:07:52 +0530

    landing page's structure added

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require('express')
+const articleRouter = require("./routes/articles")
+const mongoose = require('mongoose')
+const app=express()
+
+app.set('view engine','ejs')
+app.get('/',(req,res)=>{
+    const articles=[{
+        title:'Test Article1',
+        createdAt: new Date(),
+        description:'Test description'
+    },
+    {
+
+        title:'Test Article2',
+        createdAt: new Date(),
+        description:'Test description'
+    }]
+    res.render('articles/index',{articles:articles})
+})
+
+app.use('/articles',articleRouter)
+app.listen(3000)
