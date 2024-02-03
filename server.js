a0b364d7a037b77e44b5d96a7b39d2314822b16b
Gayatri <gayatri29mehta@gmail.com>
2024-02-03 22:28:34 +0530

    server.js modified

--- server.js
+++ server.js
@@ -1,25 +1,20 @@
 const express = require('express')
 const articleRouter = require("./routes/articles")
+const Article = require('./models/article')
 const mongoose = require('mongoose')
-const app=express()
+const methodOverride = require('method-override')
+const app = express()
 
+mongoose.connect('mongodb://localhost/bharatInternDatabase')
 
-app.set('view engine','ejs')
-
-app.get('/',(req,res)=>{
-    const articles=[{
-        title:'Test Article1',
-        createdAt: new Date(),
-        description:'Test description'
-    },
-    {
-
-        title:'Test Article2',
-        createdAt: new Date(),
-        description:'Test description'
-    }]
-    res.render('articles/index',{articles:articles})
+app.set('view engine', 'ejs')
+app.use(express.urlencoded({extended: false}))
+app.use(methodOverride('_method'))
+app.get('/', async(req, res) => {
+    const articles =await Article.find().sort({ createdAt:'desc'})
+    res.render('articles/index', { articles: articles })
 })
 
-app.use('/articles',articleRouter)
+app.use('/articles', articleRouter)
+
 app.listen(3000)
